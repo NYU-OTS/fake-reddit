eac6cc8df75af9309402382b3d9ccecd1918bcdb
Liyun Li <ll3573@ad.nyu.edu>
2018-08-16 10:34:07 -0400

    New reducers

--- app/src/reducers/index.ts
+++ app/src/reducers/index.ts
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
-import { sessionReducer } from "./session";
-import { userReducer } from "./user";
+import { combineReducers } from "redux"
+import { forumReducer } from './forum'
+import { sessionReducer } from "./session"
+import { userReducer } from "./user"
 
 export const rootReducer = combineReducers({
+  forumState: forumReducer,
   sessionState: sessionReducer,
   userState: userReducer
 });
