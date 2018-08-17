6d817c09381c2c9e2877169348f04968103786f1
Liyun Li <ll3573@ad.nyu.edu>
2018-08-17 10:13:19 -0400

    Moved file where it should be

---
+++ app/src/index-test.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import { Home } from "./components/Home/index";
+import store from "./store";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <Home />
+      </HashRouter>
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
