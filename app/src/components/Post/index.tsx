092ba57aefebb1d66314bf3407934eeca756962d
Liyun Li <ll3573@ad.nyu.edu>
2018-08-17 17:27:54 -0400

    Post Component Up

---
+++ app/src/components/Post/index.tsx
@@ -0,0 +1,81 @@
+import * as React from "react"
+import { connect } from "react-redux"
+import { withRouter } from 'react-router-dom'
+import { compose } from "recompose"
+import * as routes from '../../constants/routes'
+import { db } from "../../firebase"
+import { withAuthorization } from "../Session/withAuthorization"
+import { CommentList } from './CommentList'
+import { FormCreateComment } from './FormCreateComment'
+
+interface InterfaceState {
+    error: any;
+}
+
+class PostComponent extends React.Component<
+    {}, 
+    InterfaceState
+> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            error: '',
+        }
+    }
+
+    public componentDidMount() {
+        const { onSetPost, location }: any = this.props;
+        const path = location.pathname;
+        const prefixLength = routes.POST.length + 1;
+        const isValidPath =
+            path.startsWith(routes.POST + '/') &&
+            path.length > prefixLength;
+
+        if (isValidPath) {
+            const postId = path.substring(prefixLength);
+
+            db.onceGetPostByID(postId).then(snapshot => {
+                if (snapshot.val()) {
+                    onSetPost({
+                        ...snapshot.val(),
+                        key: postId
+                    })
+                }
+            })
+        }
+    }
+
+    public render() {
+        const { post }: any = this.props;
+        const comments = post.comments;
+
+
+        return (
+            <div>
+                <h3>{ post.subject }</h3>
+                <p>{ post.content }</p>
+                {!!comments && <CommentList comments={comments} />}
+                <h2>Comment</h2>
+                <FormCreateComment />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: any) => ({
+    post: state.postState.post
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+    onSetPost: (post: any) => dispatch({ type: "POST_SET_POST", post }),
+});
+
+const authCondition = (authUser: any) => !!authUser;
+
+export const Post = compose(
+    withAuthorization(authCondition),
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )
+)(withRouter(PostComponent));
