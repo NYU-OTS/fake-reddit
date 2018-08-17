902d124faa3f0c7fef5a5a731879ca8e20e7b4fa
Liyun Li <ll3573@ad.nyu.edu>
2018-08-17 17:26:55 -0400

    Optimized

--- app/src/reducers/post.ts
+++ app/src/reducers/post.ts
@@ -1,42 +1,16 @@
 const INITIAL_STATE = {
-    poster: {},
-    comments: {},
-    subforum: '',
+    post: {}
 }
 
-const setPoster = (state: {}, action: any) => ({
+const setPost = (state: {}, action: any) => ({
     ...state,
-    users: action.users
-})
-
-const setSubject = (state: {}, action: any) => ({
-    ...state,
-    posts: action.posts
-})
-
-const setContent = (state: {}, action: any) => ({
-    ...state,
-    subforum: action.subforum
-})
-
-const setComments = (state: {}, action: any) => ({
-    ...state,
-    comments: action.comments
+    post: action.post
 })
 
 export function postReducer(state = INITIAL_STATE, action: any) {
     switch (action.type) {
-        case "POST_SET_POSTER": {
-            return setPoster(state, action)
-        }
-        case "POST_SET_SUBJECT": {
-            return setSubject(state, action)
-        }
-        case "POST_SET_CONTENT": {
-            return setContent(state, action)
-        }
-        case "POST_SET_COMMENT": {
-            return setComments(state, action)
+        case "POST_SET_POST": {
+            return setPost(state, action)
         }
         default:
             return state;
