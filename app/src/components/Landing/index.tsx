680f4efbc1497090e95e3aff05cfc8a97c4b7716
Liyun Li <ll3573@ad.nyu.edu>
2018-08-21 10:21:38 -0400

    react thunk practice?

--- app/src/components/Landing/index.tsx
+++ app/src/components/Landing/index.tsx
@@ -6,37 +6,78 @@ import { SubforumList } from "../Forum/SubforumList";
 interface InterfaceProps {
   currentUser: any;
   onSetSubforums: any;
+  onShowNotification: any;
+  onHideNotification: any;
+  message: string;
   subforums: {};
 }
 
 const mapStateToProps = (state: any) => ({
   currentUser: state.userState.currentUser,
-  subforums: state.forumState.subforums
+  subforums: state.forumState.subforums,
+  message: state.userState.message,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
   onSetUsers: (users: any) => dispatch({ type: "SUBFORUM_SET_USERS", users }),
   onSetSubforums: (subforums: any) => dispatch({ type: "FORUM_SET_SUBFORUMS", subforums }),
+  onShowNotification: (message: string) => {
+    setTimeout(() => {
+      return dispatch({ type: "SHOW_NOTIFICATION", message })
+    }, 5000)
+  },
+  onHideNotification: () => {
+    setTimeout(() => {
+      return dispatch({ type: "HIDE_NOTIFICATION" })
+    }, 10000)
+  },
 });
 
 class LandingComponent extends React.Component<
-  InterfaceProps, {}
+  InterfaceProps, { time: any }
   > {
+
+  public timeInterval: NodeJS.Timer;
+  public notificationInterval: NodeJS.Timer;
+
+  constructor(props: Readonly<InterfaceProps>) {
+    super(props)
+    this.state = {
+      time: null
+    }
+  }
   public componentDidMount() {
-    const { onSetSubforums } = this.props
+    const {
+      onSetSubforums,
+      onShowNotification,
+      onHideNotification,
+    } = this.props
     db.onceGetSubforums().then(snapshot => {
       onSetSubforums(snapshot.val())
     })
+
+    onShowNotification('Heyyy your attention please...Nevermind!')
+    onHideNotification()
+    this.timeInterval = setInterval(() => {
+      this.setState({ time: Date.now() })
+    }, 1000);
+  }
+
+  public componentWillUnmount() {
+    clearInterval(this.timeInterval)
   }
 
   public render() {
-    const { currentUser, subforums } = this.props;
+    const { currentUser, subforums, message } = this.props;
+    const { time } = this.state;
     return (
       currentUser
         ? (
           <div>
             <h2>Subforums</h2>
-            <SubforumList subforums={subforums} />
+            <h5>{!!time && time}</h5>
+            <p>{message}</p>
+            {!!subforums && <SubforumList subforums={subforums} />}
           </div>
         )
         : (
--- app/src/reducers/user.ts
+++ app/src/reducers/user.ts
@@ -19,6 +19,16 @@ const setCurrentUser = (state: {}, action: any) => ({
   currentUser: action.currentUser
 })
 
+const showNotification = (state: {}, action: any) => ({
+  ...state,
+  message: action.message
+})
+
+const hideNotification = (state: {}) => ({
+  ...state,
+  message: ''
+})
+
 export function userReducer(state = INITIAL_STATE, action: any) {
   switch (action.type) {
     case "USER_SET_CURRENT_USER": {
@@ -29,6 +39,12 @@ export function userReducer(state = INITIAL_STATE, action: any) {
     }
     case "USER_SET_RECIPIENT": {
       return setRecipient(state, action)
+    }
+    case "SHOW_NOTIFICATION": {
+      return showNotification(state, action)
+    }
+    case "HIDE_NOTIFICATION": {
+      return hideNotification(state)
     }
     default:
       return state;
